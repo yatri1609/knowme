9669a41ed759149e3036f41c585e5d50266aee8f
Yatri Patel <yatripatel.169@gmail.com>
2018-01-07 13:25:54 +0530

    Add files via upload

---
+++ sendEmail.php
@@ -0,0 +1,39 @@
+<?php
+
+
+function Connect()
+{
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "";
+ $dbname = "responses";
+
+ // Create connection
+ $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die($conn->connect_error);
+
+ return $conn;
+}
+ 
+?>
+
+<?php
+
+require 'connection.php';
+$conn    = Connect();
+$name    = $conn->real_escape_string($_POST['u_name']);
+$email   = $conn->real_escape_string($_POST['u_email']);
+$subj    = $conn->real_escape_string($_POST['subj']);
+$message = $conn->real_escape_string($_POST['message']);
+$query   = "INSERT into tb_cform (u_name,u_email,subj,message) VALUES('" . $name . "','" . $email . "','" . $subj . "','" . $message . "')";
+$success = $conn->query($query);
+
+if (!$success) {
+    die("Couldn't enter data: ".$conn->error);
+
+}
+
+echo "Thank You For Contacting Us <br>";
+
+$conn->close();
+
+?>
